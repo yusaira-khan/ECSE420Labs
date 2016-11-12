38206958c0e932bf1df29b57a8d6d8ad7de2d3d5
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2016-11-11 20:18:19 -0500

    works but slow

--- Lab_2/grid_512_512.py
+++ Lab_2/grid_512_512.py
@@ -23,46 +23,6 @@ u1[size*size/2 + size/2] = 1
 for iteration in range(iterations):
 
 	#sides send their data to center elements and once center has updated, sides receive that value and then update
-	 
-	for elem in sides_top:
-		if (rank == elem):
-			comm.send(u1[elem], dest=(elem+size))
-			u_center = comm.recv(source=(elem+size))
-			u[elem] = G*u_center
-			u2[elem] = u1[elem]
-			u1[elem] = u[elem]
-
-	for elem in sides_right:
-		if (rank == elem):
-			comm.send(u1[elem], dest=(elem+1))
-			u_center = comm.recv(source=(elem+1))
-			u[elem] = G*u_center
-			if (elem == size):
-				comm.send(u[elem], dest=(elem-size))
-			if (elem == (size-2)*size):
-				comm.send(u[elem], dest=(elem+size))
-			u2[elem] = u1[elem]
-			u1[elem] = u[elem]
-
-	for elem in sides_left:
-		if (rank == elem):
-			comm.send(u1[elem], dest=(elem-1))
-			u_center = comm.recv(source=(elem-1))
-			u[elem] = G*u_center
-			if (elem == (2*size-1)):
-				comm.send(u[elem], dest=(elem-size))
-			if (elem == ((size-1)*size - 1)):
-				comm.send(u[elem], dest=(elem+size))
-			u2[elem] = u1[elem]
-			u1[elem] = u[elem]
-
-	for elem in sides_bottom:
-		if (rank == elem):
-			comm.send(u1[elem], dest=(elem-size))
-			u_center = comm.recv(source=(elem-size))
-			u[elem] = G*u_center
-			u2[elem] = u1[elem]
-			u1[elem] = u[elem]
 
 	#center elements send and receive data then update then send updated value to sides
 
@@ -78,13 +38,13 @@ for iteration in range(iterations):
 			u2[elem] = u1[elem]
 			u1[elem] = u[elem]
 		elem = row_index * size + 0 #right side
-		u_center =  u1[elem + 1] #because u is now u1
+		u_center =  u[elem + 1] 
 		u[elem] = G * u_center
 		u2[elem] = u1[elem]
 		u1[elem] = u[elem]
 			
 		elem += size - 1 #left side
-		u_center =  u1[elem - 1] #because u is now u1
+		u_center =  u[elem - 1] 
 		u[elem] = G * u_center
 		u2[elem] = u1[elem]
 		u1[elem] = u[elem]
@@ -105,13 +65,13 @@ for iteration in range(iterations):
 			u2[elem] = u1[elem]
 			u1[elem] = u[elem]
 		elem = row_index * size + 0 #right side
-		u_center =  u1[elem + 1] #because u is now u1
+		u_center =  u1[elem + 1]
 		u[elem] = G * u_center
 		u2[elem] = u1[elem]
 		u1[elem] = u[elem]
 			
 		elem += size - 1 #left side
-		u_center =  u1[elem - 1] #because u is now u1
+		u_center =  u1[elem - 1]
 		u[elem] = G * u_center
 		u2[elem] = u1[elem]
 		u1[elem] = u[elem]
@@ -131,28 +91,114 @@ for iteration in range(iterations):
 			u2[elem] = u1[elem]
 			u1[elem] = u[elem]
 		elem = row_index * size + 0 #right side
-		u_center =  u2[elem + 1] #because u2 is now u1
+		u_center =  u[elem + 1] #because u is now u1
 		u[elem] = G * u_center
 		u2[elem] = u1[elem]
 		u1[elem] = u[elem]
 			
 		elem += size - 1 #left side
-		u_center =  u2[elem - 1] #because u2 is now u1
+		u_center =  u[elem - 1] #because u is now u1
 		u[elem] = G * u_center
 		u2[elem] = u1[elem]
-
-	#once sides have updated, corners receive sides values and update
-	for elem in corners:
-		if (rank == elem):
-			if (elem/size == 0):
-				u_sides = comm.recv(source=elem+size)
-				u[elem] = G*u_sides
-			else:
-				u_sides = comm.recv(source=elem-size)
-				u[elem] = G*u_sides
+	
+	#top rows
+	if (rank == 0 ):
+		row_index = 0
+		for col in range(1,size-1 ):
+			elem = row_index * size + col
+			u_center =  G * u1[elem + size]
+			u[elem] = G * u_center
+			
 			u2[elem] = u1[elem]
 			u1[elem] = u[elem]
+		elem =  0 #top right corner
+		u_center =  u1[elem + size]
+		u[elem] = G * u_center
+		u2[elem] = u1[elem]
+		u1[elem] = u[elem]
+			
+		elem += size - 1 #left side
+		u_center =  u1[elem + size]
+		u[elem] = G * u_center
+		u2[elem] = u1[elem]
+		u1[elem] = u[elem]
+		
+		if num_processes != 1 :
+			#sending bottommost row
+			row_index = (rank + 1) * rows_per_process - 1
+			comm.send(u1[(row_index * size) : (row_index + 1) * size], dest=(rank + 1))
+			recvd = comm.recv(source=( rank  + 1))
+			for col in range(1, size - 1 ):
+				elem = row_index * size + col
+				u1_t =  u1[(elem - size)]
+				u1_r = u1[(elem - 1)]
+				u1_l = u1[(elem + 1)]
+				u1_b =  recvd[col]
+				u[elem] = (rho * (u1_t + u1_r + u1_l + u1_b - 4*u1[elem]) + 2*u1[elem] - (1-eta)*u2[elem])/(1+eta)
+				
+				u2[elem] = u1[elem]
+				u1[elem] = u[elem]
+			elem = row_index * size + 0 #right side
+			u_center =  u[elem + 1] #because u is now u1
+			u[elem] = G * u_center
+			u2[elem] = u1[elem]
+			u1[elem] = u[elem]
+			
+			elem += size - 1 #left side
+			u_center =  u[elem - 1] #because u is now u1
+			u[elem] = G * u_center
+			u2[elem] = u1[elem]
+			u1[elem] = u[elem]
+
+	#bottom rows
+	if (rank == num_processes - 1 ):
+		if num_processes != 1 :
+			#sending topmost
+			row_index = rank * rows_per_process + 0
+			comm.send(u1[(row_index * size) : (row_index + 1) * size], dest=(rank - 1))
+			recvd = comm.recv(source=( rank  - 1))
+			for col in range(1,size-1 ):
+				elem = row_index * size + col
+				u1_t = recvd[col] 
+				u1_r = u1[(elem-1)]
+				u1_l = u1[(elem+1)]
+				u1_b = u1[(elem+size)]
+				u[elem] = (rho * (u1_t + u1_r + u1_l + u1_b - 4*u1[elem]) + 2*u1[elem] - (1-eta)*u2[elem])/(1+eta)
+			
+				u2[elem] = u1[elem]
+				u1[elem] = u[elem]
+			elem = row_index * size + 0 #right side
+			u_center =  u[elem + 1] #because u is now u1
+			u[elem] = G * u_center
+			u2[elem] = u1[elem]
+			u1[elem] = u[elem]
+			
+			elem += size - 1 #left side
+			u_center =  u[elem - 1] #because u is now u1
+			u[elem] = G * u_center
+			u2[elem] = u1[elem]
+			u1[elem] = u[elem]
+		
+		#bottommost row
+		row_index = (rank + 1) * rows_per_process - 1
+		for col in range(1, size - 1 ):
+			elem = row_index * size + col
+			u_center =  G * u1[elem - size]
+			u[elem] = G * u_center
+				
+			u2[elem] = u1[elem]
+			u1[elem] = u[elem]
+		elem = row_index * size + 0 #right side
+		u_center =  u[elem - size] #because u is now u1
+		u[elem] = G * u_center
+		u2[elem] = u1[elem]
+		u1[elem] = u[elem]
+			
+		elem += size - 1 #left side
+		u_center =  u[elem - size] #because u is now u1
+		u[elem] = G * u_center
+		u2[elem] = u1[elem]
+		u1[elem] = u[elem]
+			
 	
-#	print rank, u
-#	print rank, u2, u1
 
