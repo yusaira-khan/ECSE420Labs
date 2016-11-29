81a27d51bdebd8e486822691a7426c8dd0cc80df
IRNiti <ioana.niti@mail.mcgill.ca>
2016-11-29 15:49:41 -0500

    lab2 q1 working

--- Lab_3/grid_4_4.cu
+++ Lab_3/grid_4_4.cu
@@ -1,140 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void updateCenter(float *d_out, float *d1_in, float *d2_in) {
-	int idx = threadIdx.x;
+__global__ void update(float *d_out, float *d_in) {
+	int idx = blockDim.x * blockIdx.x + threadIdx.x;
 	float rho = 0.5;
 	float eta = 0.0002;
+	float G = 0.75;
 	int size = 4;
-	printf("u1: ");
-	for(int i=0; i<size*size; i++){
-		printf("%f, ", d1_in[i]);
-	}
-	printf("\nu2: ");
-	for(int i=0; i<size*size; i++){
-		printf("%f, ", d2_in[i]);
-	}
-	printf("\n");
+	
+	//update center
 	if((idx/size != 0) && (idx/size != 3) && (idx%size != 0) && (idx%size != 3)) {
-		d_out[idx] = (rho*(d1_in[idx-1] + d1_in[idx+1] + d1_in[idx-size] +d1_in[idx+size] - 4*d1_in[idx]) + 2*d1_in[idx] - (1-eta)*d2_in[idx])/(1+eta);
-	} else {
-		d_out[idx] = 0;
+		d_out[idx] = (rho*(d_in[idx-1] + d_in[idx+1] + d_in[idx-size] +d_in[idx+size] - 4*d_in[idx]) + 2*d_in[idx] - (1-eta)*d_in[idx+size*size])/(1+eta);
 	}
 	
-//	printf("updating center and idx is %d, u[idx] is %f, u1[idx-1] is %f, u1[idx+1] is %f, u1[idx-size] is %f, u1[idx+size] is %f, u1[idx] is %f and u2[idx] is %f\n", idx, d_out[idx], d1_in[idx-1], d1_in[idx+1], d1_in[idx-size], d1_in[idx+size], d1_in[idx], d2_in[idx]);
-
-}
-
-__global__ void updateSides(float *d_out, float *d_in){
-	int idx = threadIdx.x;
-	int size = 4;
-	float G = 0.75;
-
-	if((idx/size == 0) && (idx != 0) && (idx != (size -1))){
-		d_out[idx] = G*d_in[idx + size];
+	//update sides
+	else if((idx/size == 0) && (idx != 0) && (idx != (size -1))){
+		d_out[idx] = G*(rho*(d_in[idx-1+size] + d_in[idx+1+size] + d_in[idx-size+size] +d_in[idx+size+size] - 4*d_in[idx+size]) + 2*d_in[idx+size] - (1-eta)*d_in[idx+size*size+size])/(1+eta);
 	}
 	else if((idx/size == 3) && (idx != (size-1)*size) && (idx != (size*size - 1))){
-		d_out[idx] = G*d_in[idx - size];
+		d_out[idx] = G*(rho*(d_in[idx-1-size] + d_in[idx+1-size] + d_in[idx-size-size] +d_in[idx+size-size] - 4*d_in[idx-size]) + 2*d_in[idx-size] - (1-eta)*d_in[idx+size*size-size])/(1+eta);
 	}
 	
 	else if ((idx%size == 0) && (idx != 0) && (idx != (size-1)*size)){
-		d_out[idx] = G*d_in[idx + 1];
+		d_out[idx] = G*(rho*(d_in[idx-1+1] + d_in[idx+1+1] + d_in[idx-size+1] +d_in[idx+size+1] - 4*d_in[idx+1]) + 2*d_in[idx+1] - (1-eta)*d_in[idx+size*size+1])/(1+eta);
 	}
 	
 	else if((idx%size == 3) && (idx != (size -1)) && (idx != (size*size - 1))){
-		d_out[idx] = G*d_in[idx - 1];
-	}
-	else {
-		d_out[idx] = d_in[idx];
+		d_out[idx] = G*(rho*(d_in[idx-1-1] + d_in[idx+1-1] + d_in[idx-size-1] +d_in[idx+size-1] - 4*d_in[idx-1]) + 2*d_in[idx-1] - (1-eta)*d_in[idx+size*size-1])/(1+eta);
 	}
 	
-//	printf("updating sides and idx is %d, and u[idx] is %f\n", idx, d_out[idx]);
-
-}
-
-__global__ void updateCorners(float *d_out, float *d_in){
-	int idx = threadIdx.x;
-	int size = 4;
-	float G = 0.75;
-
-	if ((idx == 0) || (idx == (size - 1))){
-		d_out[idx] = G*d_in[idx + size];
-	} else if ((idx == (size-1)*size) || (idx == (size*size - 1))){
-		d_out[idx] = G*d_in[idx - size];
-	} else {
-		d_out[idx] = d_in[idx];
+	//update corners
+	else if (idx == 0){
+		d_out[idx] = G*G*(rho*(d_in[idx-1+size+1] + d_in[idx+1+size+1] + d_in[idx-size+size+1] +d_in[idx+size+size+1] - 4*d_in[idx+size+1]) + 2*d_in[idx+size+1] - (1-eta)*d_in[idx+size*size+size+1])/(1+eta);
+	} else if(idx == (size - 1)){
+		d_out[idx] = G*G*(rho*(d_in[idx-1+size-1] + d_in[idx+1+size-1] + d_in[idx-size+size-1] +d_in[idx+size+size-1] - 4*d_in[idx+size-1]) + 2*d_in[idx+size-1] - (1-eta)*d_in[idx+size*size+size-1])/(1+eta);
+	} else if (idx == (size-1)*size){
+		d_out[idx] = G*G*(rho*(d_in[idx-1-size+1] + d_in[idx+1-size+1] + d_in[idx-size-size+1] +d_in[idx+size-size+1] - 4*d_in[idx-size+1]) + 2*d_in[idx-size+1] - (1-eta)*d_in[idx+size*size-size+1])/(1+eta);
+	} else if (idx == (size*size - 1)){
+		d_out[idx] = G*G*(rho*(d_in[idx-1-size-1] + d_in[idx+1-size-1] + d_in[idx-size-size-1] +d_in[idx+size-size-1] - 4*d_in[idx-size-1]) + 2*d_in[idx-size-1] - (1-eta)*d_in[idx+size*size-size-1])/(1+eta);
 	}
-	
-//	printf("updating corners and idx is %d, and u[idx] is %f\n", idx, d_out[idx]);
-
 }
 
 int main(int argc, char **argv){
 	int iterations = atoi(argv[1]);
 	int size = 16;
-	float u2[size];
-	float u1[size];
+	int size_bytes = size*sizeof(float);
+	float u1_2[2*size];
 	float u[size];
-	float *d1_in, *d2_in, *d_in, *d_out;
-/*	float *center_in, *center_out;
-	float *sides_in, *sides_out;
-	float *corners_in, *corners_out;
-	int center_size = 7;
-	int sides_size = 4;
-	int corners_size = 4;
-*///initialize arrays
+	float *d_in, *d_out;
+
+//initialize arrays
+	for(int i=0; i<2*size; i++){
+		if (i==10)
+			u1_2[i] = 1;
+		else
+			u1_2[i] = 0;
+	}
+	
 	for(int i=0; i<size; i++){
-	//	for(int j=0; j<4; j++){
-			u2[i] = 0;
-			if (i==10)
-				u1[i] = 1;
-			else
-				u1[i] = 0;
-	//	}
+		u[i] = 0;
 	}
 
-/*	cudaMalloc(&center_in, 7);
-	cudaMalloc(&center_out, 1);
-	cudaMalloc(&sides_in, 4);
-	cudaMalloc(&sides_out, 8);
-	cudaMalloc(&corners_in, 4);
-	cudaMalloc(&corners_out, 4);
-*/	cudaMalloc(&d1_in, size);
-	cudaMalloc(&d2_in, size);
-	cudaMalloc(&d_out, size);
-	cudaMalloc(&d_in, size);
+	cudaMalloc(&d_in, 2*size_bytes);
+	cudaMalloc(&d_out, size_bytes);
 	
-//	for(int i=0; i<iterations; i++){
-	cudaMemcpy(d1_in, u1, size, cudaMemcpyHostToDevice);
-	cudaMemcpy(d2_in, u2, size, cudaMemcpyHostToDevice);
+	for(int i=0; i<iterations; i++){
+		cudaMemcpy(d_in, u1_2, 2*size_bytes, cudaMemcpyHostToDevice);
 
-	updateCenter<<<1, size>>>(d_out, d1_in, d2_in);
+		update<<<size, 1>>>(d_out, d_in);
 
-	cudaMemcpy(u, d_out, size, cudaMemcpyDeviceToHost);	
+		cudaMemcpy(u, d_out, size_bytes, cudaMemcpyDeviceToHost);	
+		
+		for(int j=0; j<size; j++){
+			u1_2[j+size] = u1_2[j];
+		}
 
-	cudaMemcpy(d_in, u, size, cudaMemcpyHostToDevice);
-
-	updateSides<<<1, size>>>(d_out, d_in);
-
-	cudaMemcpy(u, d_out, size, cudaMemcpyDeviceToHost);
-
-	cudaMemcpy(d_in, u, size, cudaMemcpyHostToDevice);
-
-	updateCorners<<<1, size>>>(d_out, d_in);
-
-	cudaMemcpy(u, d_out, size, cudaMemcpyDeviceToHost);
-	
-	for(int j=0; j<size; j++){
-		u2[j] = u1[j];
-	}
-
-	for(int j=0; j<size; j++){
-		u1[j] = u[j];
+		for(int j=0; j<size; j++){
+			u1_2[j] = u[j];
+		}
+		
+		printf("%f\n", u[10]);
 	}
 	
-	for(int j=0; j<size; j++){
-		printf("%f, ", u[j]);
-	}
-	printf("\n");
-//	}
+	cudaFree(d_in);
+	cudaFree(d_out);
 }
